f9982454ac5e5da84d5aa625a3fdbd981ca9872a
Void787 <1195841@student.roc-nijmegen.nl>
2023-05-09 11:27:03 +0200

    5.1 : ik heb de template voor ClockHand gebruikt als basis en heb de to string aangepast om ook de seconden te laten zien en in Advance heb ik seconds++ gedaan om zo de seconden bij te houden ook heb ik het gemaakt dat hij bij 60:00 (oftewel 1 minuut) het programma afsluit.

--- C-opdrachten/Program.cs
+++ C-opdrachten/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace C_opdrachten
+﻿using System.Runtime.CompilerServices;
+
+namespace C_opdrachten
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create new timer
+            Timer timer = new(100);
+            bool issixty = false;
+            // Loop until you cancel the loop.
+            // You can cancel with the CTRL + C
+            while (issixty == false)
+            {
+                issixty = timer.StopTimer();
+                Console.WriteLine(timer);
+                timer.Advance();
+                // Some error proving, we'll talk about this later.
+                // Known as try-catch.
+                try
+                {
+                    // Wait 100th of a second. 
+                    // Sleep(1000) waits one second, if you want to test at slower pace.
+                    Thread.Sleep(10);
+                }
+                // Other half of try-catch pair. 
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error happened: +" + e);
+                }
+            }
         }
     }
 }
---
+++ C-opdrachten/Timer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_opdrachten
+{
+    internal class Timer
+    {
+        public int value { get; set; }
+        public int limit { get; set; }
+        public int seconds { get; set; }
+
+        public Timer(int limit)
+        {
+            this.limit = limit;
+            this.value = 0;
+        }
+
+        public void Advance()
+        {
+            this.value++;
+
+            if (this.value >= this.limit)
+            {
+                this.value = 0;
+                this.seconds++;
+            }
+        }
+
+        public override string ToString()
+        {
+            string miliseconds = "";
+            string seconds = "";
+            if (this.value < 10)
+            {
+                miliseconds = "0" + this.value;
+            } else { 
+            miliseconds = this.value.ToString();
+            }
+            if (this.seconds < 10)
+            {
+                seconds = "0" + this.seconds;
+            } else {  seconds = this.seconds.ToString(); }
+
+            return seconds + ":" + miliseconds;
+        }
+
+        public bool StopTimer()
+        {
+            if (this.seconds >= 60)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
