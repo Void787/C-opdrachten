b648b6dc8afe1be682cffb3a6a0114746b66c9b2
Void787 <1195841@student.roc-nijmegen.nl>
2023-05-11 11:45:13 +0200

    opdracht 5.4: Ik heb 3 constructers gemaakt en de zin laten zien door een override string tostring in de Product class te zetten

---
+++ opdracht 5.4/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht_5._4
+{
+    internal class Product
+    {
+        public string name;
+        public string location;
+        public int weight;
+
+        public Product(string name) 
+        { 
+            this.name = name;
+            this.location = "shelf";
+            this.weight = 1;
+        }
+
+        public Product(string name, string location)
+        {
+            this.name = name;
+            this.location = location;
+            this.weight = 1;
+        }
+
+        public Product(string name, int weight)
+        {
+            this.name = name;
+            this.location = "warehouse";
+            this.weight = weight;
+        }
+
+        public override string ToString()
+        {
+            string zin = name + " (" + weight + " kg) can be found from the " + location;
+            return zin;
+        }
+    }
+}
---
+++ opdracht 5.4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace opdracht_5._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product tapeMeasure = new Product("Tape measure");
+            Product plaster = new Product("Plaster", "home improvement section");
+            Product tyre = new Product("Tyre", 5);
+
+            Console.WriteLine(tapeMeasure);
+            Console.WriteLine(plaster);
+            Console.WriteLine(tyre);
+        }
+    }
+}
